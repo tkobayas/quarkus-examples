18bf18a29d4d40ec6a1eddfd919d3d397a6f2b43
Toshiya Kobayashi <toshiyakobayashi@gmail.com>
2021-12-03 16:32:50 +0900

    geeting-extension

---
+++ quarkus-extensions/greeting-extension/deployment/src/main/java/org/acme/greeting/extension/deployment/GreetingExtensionProcessor.java
@@ -0,0 +1,25 @@
+package org.acme.greeting.extension.deployment;
+
+import org.acme.greeting.extension.GreetingExtensionServlet;
+
+import io.quarkus.deployment.annotations.BuildStep;
+import io.quarkus.deployment.builditem.FeatureBuildItem;
+import io.quarkus.undertow.deployment.ServletBuildItem;
+
+class GreetingExtensionProcessor {
+
+    private static final String FEATURE = "greeting-extension";
+
+    @BuildStep
+    FeatureBuildItem feature() {
+        return new FeatureBuildItem(FEATURE);
+    }
+
+    @BuildStep
+    ServletBuildItem createServlet() { 
+      ServletBuildItem servletBuildItem = ServletBuildItem.builder("greeting-extension", GreetingExtensionServlet.class.getName())
+        .addMapping("/greeting")
+        .build(); 
+      return servletBuildItem;
+    }
+}
